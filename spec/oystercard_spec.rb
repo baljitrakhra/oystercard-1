00d47cf22be8ddaf23a8d67072c6a92933e9b62b
Coral-Ann <coral.baker@outlook.com>
2022-03-28 16:22:51 +0100

    Added/tested top_up method.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,11 +1,12 @@
 require_relative '../lib/oystercard'
 
 describe Oystercard do
-  context "#balance" do
-    it { is_expected.to respond_to :balance }
+  it 'starts with a balance of 0.0' do
+    expect(subject.balance).to eq 0.0
+  end
 
-    it 'starts with a balance of 0.0' do
-      expect(subject.balance).to eq 0.0
-    end
+  it 'should add an amount to balance.' do
+    expect { subject.top_up(30) }.to change { subject.balance }.by(30.0)
   end
 end
+
