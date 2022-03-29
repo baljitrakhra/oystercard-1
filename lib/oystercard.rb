9b69891fe3eda98fd448f9c049aba376f47aaad7
Joe McNally <joe.t.mcnally@hotmail.com>
2022-03-29 16:36:43 +0100

    Created and passed tests for storing journeys made with the oystercard into an instance variable array. Also updated other methods that will use this array.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
   DEFAULT_LIMIT = 90.0
   MINIMUM_FARE = 1.0
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :exit_station, :list_of_journeys
 
-  def initialize( station = nil)
+  def initialize()
     @balance = 0.0
-    @entry_station = station
+    @entry_station = nil
+    @exit_station = nil
+    @list_of_journeys = []
   end
 
   def top_up(amount)
@@ -22,8 +24,10 @@ class Oystercard
     @entry_station = station
   end
 
-  def touch_out
+  def touch_out(station)
     self.deduct(MINIMUM_FARE)
+    @exit_station = station
+    add_journey
     @entry_station = nil
   end
 
@@ -31,5 +35,10 @@ class Oystercard
 
   def deduct(amount)
     @balance -= amount.to_f
+  end
+
+  def add_journey
+    journey = {entry_station: @entry_station, exit_station: @exit_station}
+    list_of_journeys.push(journey)
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,23 +5,23 @@ describe Oystercard do
 
   describe '#balance' do
     it 'starts with a balance of 0.0' do
-      expect(subject.balance).to eq 0.0
+      expect(card.balance).to eq 0.0
     end
   end
 
   describe '#top_up' do
     it 'should add an amount to balance' do
-      expect { subject.top_up(30) }.to change { subject.balance }.by(30.0)
+      expect { card.top_up(30) }.to change { card.balance }.by(30.0)
     end
 
     it 'should limit the card to default limit' do
-      expect { subject.top_up(100) }.to raise_error "Cannot exceed #{Oystercard::DEFAULT_LIMIT} limit"
+      expect { card.top_up(100) }.to raise_error "Cannot exceed #{Oystercard::DEFAULT_LIMIT} limit"
     end
   end
 
   describe '#in_journey?' do
     it 'should check in use or out of use' do
-      expect(subject.in_journey?).to eq false
+      expect(card.in_journey?).to eq false
     end
   end
 
@@ -36,25 +36,50 @@ describe Oystercard do
     end
 
     it 'should raise an error if balance is below minimum fare' do
-      expect { subject.touch_in(station) }.to raise_error "Below minimum fare"
+      expect { card.touch_in(station) }.to raise_error "Below minimum fare"
     end
 
   end
 
   describe '#touch_out' do
-    let (:station) {double :station}
+    let (:station_entry) {double :station}
+    let (:station_exit) {double :station}
+
     before do
-      subject.top_up(30)
-      subject.touch_in(station)
-      
+      card.top_up(30)
+      card.touch_in(station_entry)
     end
 
+    it {is_expected.to respond_to(:touch_out).with(1).argument}
+
     it 'should change card state to out of use' do
-      expect { subject.touch_out }.to change { subject.in_journey? }.to(false)
+      expect { card.touch_out(station_exit) }.to change { card.in_journey? }.to(false)
     end
 
     it 'should deduct the minimum fare' do
-      expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_FARE)
+      expect { card.touch_out(station_exit) }.to change { card.balance }.by(-Oystercard::MINIMUM_FARE)
+    end
+
+    it 'will tell you which station you have exited from' do
+      expect { card.touch_out(station_exit) }.to change { card.exit_station}.to station_exit
+    end
+  end
+
+  describe '#list_of_journeys' do
+  let (:station_entry) {double :station}
+  let (:station_exit) {double :station}
+  let (:journey) {{entry_station: station_entry, exit_station: station_exit}}
+
+    it 'gives an empty array by default' do
+      expect(card.list_of_journeys).to eq []
+    end
+
+    it 'stores a journey' do
+      card.top_up(30)
+      card.touch_in(station_entry)
+      card.touch_out(station_exit)
+      expect(card.list_of_journeys).to include journey
+      expect(card.list_of_journeys.length).to eq 1
     end
   end
 end
