647850ded3ab7f5b0065360047212759e8ed753a
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-29 17:07:08 +0100

    added a station class which accept name and zone

---
+++ spec/station_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/station'
+
+describe 'Station' do
+  station = Station.new("Paddington","one")
+  describe 'initializes station with name and zone' do
+    it 'add name to the station' do
+      expect(station.name).to eq "Paddington"
+    end
+    it 'add zone to the station' do
+      expect(station.zone).to eq "one"
+    end
+  end
+end
