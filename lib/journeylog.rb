020914c6d6a9f0f9a3048d97e6653fe384308d69
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-03-31 15:40:58 +0100

    added start method

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -2,9 +2,14 @@ require_relative 'journey'
 
 class JourneyLog
 
-  attr_reader :journey
+  attr_reader :journey, :list_of_journeys, :start_journey
 
   def initialize(journey)
     @journey = journey
+    @list_of_journeys = []
+  end
+
+  def start(station)
+    @start_journey = Journey.new(station)
   end
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -12,5 +12,17 @@ describe JourneyLog do
       p journeylog = JourneyLog.new(Journey.new(nil))
       expect(journeylog.journey).to be_an_instance_of(Journey)
     end
+    it 'creates a list of journey and at start it hould be empty' do
+      expect(journeylog.list_of_journeys).to eq []
+    end
   end
+  
+  context '#start_journey' do
+    let(:entry_station) {double :station}
+    let(:journeylog) { JourneyLog.new(Journey.new(nil))}
+    it 'starts a journey' do
+      expect(journeylog.start(entry_station)).to eq journeylog.start_journey
+    end
+  end
+
 end
